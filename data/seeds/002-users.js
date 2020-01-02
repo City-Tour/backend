015670720ef21c3459c5e4ad9933027267255305
Alexis Carr <acarr59@gmail.com>
2020-01-02 17:48:56 -0600

    📦 Finishes suggestionsRouter but not tested

--- data/seeds/002-users.js
+++ data/seeds/002-users.js
@@ -2,13 +2,32 @@ const bcrypt = require('bcryptjs')
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users').truncate()
-    .then(function () {
+  return knex('users')
+    .truncate()
+    .then(function() {
       // Inserts seed entries
       return knex('users').insert([
-        {id: 1, email: 'test@test.com', password: bcrypt.hashSync('catz', 12), first_name: 'Alexis', last_name: 'Carr'},
-        {id: 2, email: 'rowValue2', password: bcrypt.hashSync('catz', 12), first_name: 'Earl', last_name: 'Cat'},
-        {id: 3, email: 'rowValue3', password: bcrypt.hashSync('catz', 12), first_name: 'Birla', last_name: 'Zat'}
-      ]);
-    });
-};
+        {
+          id: 1,
+          email: 'test@test.com',
+          password: bcrypt.hashSync('catz', 12),
+          first_name: 'Alexis',
+          last_name: 'Carr'
+        },
+        {
+          id: 2,
+          email: 'rowValue2',
+          password: bcrypt.hashSync('catz', 12),
+          first_name: 'Earl',
+          last_name: 'Cat'
+        },
+        {
+          id: 3,
+          email: 'rowValue3',
+          password: bcrypt.hashSync('catz', 12),
+          first_name: 'Birla',
+          last_name: 'Zat'
+        }
+      ])
+    })
+}
