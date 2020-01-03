cbd96273ee68e1b84f6e353aafb792a54db2175c
Alexis Carr <acarr59@gmail.com>
2020-01-03 05:38:18 -0600

    🌱 Updates seeds for packages

--- data/seeds/003-packages.js
+++ data/seeds/003-packages.js
@@ -8,7 +8,7 @@ exports.seed = function(knex) {
         {
           package_id: 1,
           name: 'Must Haves',
-          type: '1d',
+          type: 'One Day Package',
           description: 'Top places in Nashville, period',
           price: 120.34,
           city_id: 1,
@@ -17,7 +17,7 @@ exports.seed = function(knex) {
         {
           package_id: 2,
           name: 'Downtown Tour',
-          type: '2d',
+          type: 'Two Day Package',
           description: 'Must go to places near down town Nashville',
           price: 9.99,
           city_id: 1,
@@ -26,7 +26,7 @@ exports.seed = function(knex) {
         {
           package_id: 3,
           name: 'Nashville KUNTRY',
-          type: 'weekend',
+          type: 'Weekend',
           description: 'Real tok, mayne',
           price: 120.34,
           city_id: 1,
@@ -36,7 +36,7 @@ exports.seed = function(knex) {
         {
           package_id: 4,
           name: 'Golden Gate Tour',
-          type: '1d',
+          type: 'One Day Package',
           description: 'Places around the Golden Gate Bridge',
           price: 9.99,
           city_id: 2,
@@ -45,7 +45,7 @@ exports.seed = function(knex) {
         {
           package_id: 5,
           name: 'Downtown San Fran',
-          type: '2d',
+          type: 'Two Day Package',
           description: 'Must go to places near down town San Fransisco',
           price: 19.99,
           city_id: 2,
@@ -54,7 +54,7 @@ exports.seed = function(knex) {
         {
           package_id: 6,
           name: 'City Tour',
-          type: 'weekend',
+          type: 'Weekend',
           description: 'Best Town Tour',
           price: 29.99,
           city_id: 2,
@@ -64,7 +64,7 @@ exports.seed = function(knex) {
         {
           package_id: 7,
           name: 'Remember the Alamo',
-          type: '1d',
+          type: 'One Day Package',
           description: 'Alamo Tour',
           price: 9.99,
           city_id: 3,
@@ -73,7 +73,7 @@ exports.seed = function(knex) {
         {
           package_id: 8,
           name: 'Downtown Austin',
-          type: '2d',
+          type: 'Two Day Package',
           description: 'Must go to places near down town Austin',
           price: 19.99,
           city_id: 3,
@@ -82,7 +82,7 @@ exports.seed = function(knex) {
         {
           package_id: 9,
           name: 'Autin City Tour',
-          type: 'weekend',
+          type: 'Weekend',
           description: 'Best Around the Town Tour',
           price: 29.99,
           city_id: 3,
